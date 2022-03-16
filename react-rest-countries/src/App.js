21c357f040f2545fc01f2e3ceacad099792efb4f
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-16 23:22:31 +0600

    recap core concept again

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h2>React Rest Countries</h2>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className="App">
+            <h2>Rest Countries</h2>
+        </div>
+    )
 }
 
-export default App;
+export default App
