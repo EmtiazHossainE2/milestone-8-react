4cf74608cab94446c3e7401f9707a733f12fe29b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-16 23:54:14 +0600

    get rest countries data

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 
 const App = () => {
     return (
         <div className="App">
-            <h2>Rest Countries</h2>
+            <LoadCountries></LoadCountries>
+        </div>
+    )
+}
+const LoadCountries = () => {
+    const [countries, setCountries] = useState([])
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+    return (
+        <div>
+            <h2>Visit Every Country Invest No Money !!</h2>
         </div>
     )
 }
