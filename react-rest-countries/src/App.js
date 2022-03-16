0f60e9c02400b00b4ac20132f23f0409cc19a8f2
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-17 00:31:02 +0600

    folder structure

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -14,11 +14,26 @@ const LoadCountries = () => {
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setCountries(data))
     }, [])
     return (
         <div>
             <h2>Visit Every Country Invest No Money !!</h2>
+            <h3>Available Country : {countries.length}</h3>
+            {
+                countries.map(country =>
+                    <Country Name={country.name.common} Population={country.population} Flag={country.flags.png}></Country>
+                )
+            }
+        </div>
+    )
+}
+function Country(props) {
+    return (
+        <div className='single-style'>
+            <h2>Name : {props.Name}</h2>
+            <p>Population : {props.Population}</p>
+            <img src={props.Flag} alt="" />
         </div>
     )
 }
