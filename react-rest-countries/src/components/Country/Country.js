4da308e58a426c902bdae55bc97420eccedc2ce0
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-17 16:46:16 +0600

    google map added

--- react-rest-countries/src/components/Country/Country.js
+++ react-rest-countries/src/components/Country/Country.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Country.css'
 const Country = (props) => {
-    const { name, population, area, flags } = props.country
+    console.log(props.country);
+    const { name, population, area, flags, maps } = props.country
     return (
         <div className='country'>
             <h3>Name : {name.common}</h3>
             <p>Total Population : {population}</p>
             <p>Total Area : {area}</p>
             <img src={flags.png} alt="" />
+            <button> <a target="_blank" rel="noopener noreferrer" href={maps.googleMaps}> Google Maps</a></button>
         </div>
     );
 };
