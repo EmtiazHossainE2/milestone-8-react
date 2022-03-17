1365285797c35a6e342412abd72d268d163d909e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-17 15:03:53 +0600

    Pass multiple data to a child component

--- react-rest-countries/src/components/Country/Country.js
+++ react-rest-countries/src/components/Country/Country.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Country.css'
 const Country = (props) => {
+    const { name, population, area, flags } = props.country
     return (
         <div className='country'>
-            <h3>Name : {props.Name}</h3>
-            <p>Total Population : {props.Population}</p>
-            <img src={props.Flag} alt="" />
+            <h3>Name : {name.common}</h3>
+            <p>Total Population : {population}</p>
+            <p>Total Area : {area}</p>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
