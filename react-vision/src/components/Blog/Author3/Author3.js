e9d54815dd991c6cb391d9aed5b3bcf34fc2c1d5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-18 00:44:28 +0600

    blog component done

---
+++ react-vision/src/components/Blog/Author3/Author3.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Author3 = () => {
+    return (
+        <div>
+            <div>
+                <h2>Manager</h2>
+                <h4>DoraEmon</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Author3;
