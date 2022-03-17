779576481d8e183e0f91f3fdade31ce4d6ab4f12
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-17 23:15:02 +0600

    react vision app initialize

---
+++ react-vision/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+
+//তুমি create react app দিয়ে রিএক্ট এর প্রজেক্ট বানাতে পারো কিনা  
+
+const App = () => {
+    return (
+        <div className='App'>
+            <h2>React Vision Day </h2>
+
+        </div>
+    );
+};
+
+export default App;
