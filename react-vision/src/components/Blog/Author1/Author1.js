d2bf56ab1fe8562078936f2401c2fd613b586039
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-18 00:44:28 +0600

    blog component done

---
+++ react-vision/src/components/Blog/Author1/Author1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Author1 = () => {
+    return (
+        <div>
+            <div>
+                <h2>Worker</h2>
+                <h4>Emtiaz Hossain Emon</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Author1;
