913ca013e308e09b7ec5c1e5b36af3d0ad4ed896
Emtiaz Hossain Emon <97201858+EmtiazHossainE2@users.noreply.github.com>
2022-03-18 04:12:45 +0600

    Revert "battery charge task complete"
    This reverts commit 513512d61f487b4766ee10f713172a2da27e0945.

--- react-vision/src/components/Blog/Blog.js
+++ react-vision/src/components/Blog/Blog.js
@@ -2,21 +2,20 @@ import React from 'react';
 import Author1 from './Author1/Author1';
 import Author2 from './Author2/Author2';
 import Author3 from './Author3/Author3';
-import './Blog.css'
 
 const Blog = () => {
     return (
-        <div className=' container blog-container pt-3'>
+        <div>
             <h2 className='text-center mt-2 mb-5'>Our Honorable Author </h2>
-            <div >
+            <div className="container">
                 <div className="row text-center my-5">
-                    <div className="col-md-4 py-4 rounded text-light" id='author1'>
+                    <div className="col-md-4">
                         <Author1></Author1>
                     </div>
-                    <div className="col-md-4 py-4 rounded text-light" id='author2'>
+                    <div className="col-md-4">
                         <Author2></Author2>
                     </div>
-                    <div className="col-md-4 py-4 rounded text-light" id='author3'>
+                    <div className="col-md-4">
                         <Author3></Author3>
                     </div>
                 </div>
