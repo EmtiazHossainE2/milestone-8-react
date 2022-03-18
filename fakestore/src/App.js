ac0cb4bcc091f0385b4588cdb7041a3982c73870
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-19 02:43:33 +0600

    menu added

--- fakestore/src/App.js
+++ fakestore/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
     return (
-        <div>
-            <h2>FakeStore </h2>
+        <div className='text-center'>
+            <h2 >FakeStore </h2>
+            <button className=' btn btn-primary'>Click me</button>
         </div >
     );
 };
