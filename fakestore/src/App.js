0280eaee46264c037abba818265d312f830cc93e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-19 22:50:26 +0600

    animation try

--- fakestore/src/App.js
+++ fakestore/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home';
+// import { AOS } from 'aos';
+// import "aos/dist/aos.css";
 const App = () => {
     const [count, setCount] = useState(0)
     const addCart = () => setCount(count + 1);
@@ -12,6 +14,9 @@ const App = () => {
             setCount(count - 1)
         }
     }
+    // useEffect(() => {
+    //     AOS.init();
+    // }, [])
     return (
         <div >
             <Home count={count} addCart={addCart} deleteCart={deleteCart}></Home>
--- fakestore/src/Components/AllProducts/Product/Product.js
+++ fakestore/src/Components/AllProducts/Product/Product.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Modal from '../../Modal/Modal';
+// import Modal from '../../Modal/Modal';
 import './Product.css'
 const Product = (props) => {
     const { addCart } = props
     const { image, title, price } = props.product
     return (
-        <div className="col-md-4 ">
+        <div data-aos="fade-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500" className="col-md-4 ">
             <div className="cart p-2 m-3 bg text-center">
                 <img className='w-50  m-auto  rounded p-1 ' src={image} alt="" />
                 <p className=' '>{title.slice(0, 20)}</p>
