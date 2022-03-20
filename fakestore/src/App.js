9781321203a1721b2dd3c29b5a0e174ff10a53f1
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-20 12:59:47 +0600

    cart fixed

--- fakestore/src/App.js
+++ fakestore/src/App.js
@@ -1,25 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Pages/Home/Home';
-// import { AOS } from 'aos';
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Cart from './Components/Cart/Cart';
 const App = () => {
-    const [count, setCount] = useState(0)
-    const addCart = () => setCount(count + 1);
-    const deleteCart = () => {
-        if (count <= 0) {
-            return
-        }
-        else {
-            setCount(count - 1)
-        }
-    }
-    // useEffect(() => {
-    //     AOS.init();
-    // }, [])
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
         <div >
-            <Home count={count} addCart={addCart} deleteCart={deleteCart}></Home>
+            <Cart></Cart>
         </div >
     );
 };
