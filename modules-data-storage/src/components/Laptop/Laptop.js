3fcfb8329503a9d2f6ca1a8deb8526d5d65d0350
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-21 12:11:14 +0600

    create event handler with parameters

--- modules-data-storage/src/components/Laptop/Laptop.js
+++ modules-data-storage/src/components/Laptop/Laptop.js
@@ -3,6 +3,10 @@ import './Laptop.css'
 
 const Laptop = (props) => {
     const { name, price, picture, company, id } = props.laptop
+    const addToCart = (id, name) => {
+        console.log('Added', id, name);
+        //Create Event handler with parameters
+    }
     return (
         <div className='single-laptop'>
             <h2>Name : {name}</h2>
@@ -10,6 +14,7 @@ const Laptop = (props) => {
             <h5>CPU : {picture}</h5>
             <strong>GPU : {company}</strong>
             <p><small>id : {id}</small></p>
+            <button onClick={() => addToCart(id, name)}>Add Cart</button>
         </div>
     );
 };
