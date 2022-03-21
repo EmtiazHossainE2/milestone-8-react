a96578980590203aecf04a10022908b83d7ae719
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-21 23:54:53 +0600

    reduce

--- modules-data-storage/src/components/utilities/fakedb.js
+++ modules-data-storage/src/components/utilities/fakedb.js
@@ -1,5 +1,4 @@
 
-
 const addToDb = id => {
     let shoppingCart = {};
     //get the shopping cart (second part ) 
@@ -32,25 +31,3 @@ const removeFromDb = id => {
 
 
 export { addToDb, removeFromDb }
-
-
-
-
-
-
-
-
-/* const addToDb = id => {
-
-    const quantity = localStorage.getItem(id)
-    if (quantity) {
-        // const newQuantity = parseInt(quantity) + 1; parseInt diye num kora jay abar samne + diyeo kora jay 
-        const newQuantity = +(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
-    }
-    else {
-        // console.log('new added ');
-        localStorage.setItem(id, 1)
-    }
-}
-export { addToDb } */
