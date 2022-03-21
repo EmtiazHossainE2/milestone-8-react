71a41333fd753119c87d273676c120b0abbd2cc8
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-21 10:50:34 +0600

    data generator

--- modules-data-storage/src/components/Laptop/Laptop.js
+++ modules-data-storage/src/components/Laptop/Laptop.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Laptop.css'
 
 const Laptop = (props) => {
-    const { name, price, CPU, GPU, id } = props.laptop
+    const { name, price, picture, company, id } = props.laptop
     return (
         <div className='single-laptop'>
             <h2>Name : {name}</h2>
             <p>Price : {price}</p>
-            <h5>CPU : {CPU}</h5>
-            <strong>GPU : {GPU}</strong>
+            <h5>CPU : {picture}</h5>
+            <strong>GPU : {company}</strong>
             <p><small>id : {id}</small></p>
         </div>
     );
--- modules-data-storage/src/components/Laptops/Laptops.js
+++ modules-data-storage/src/components/Laptops/Laptops.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Laptop from '../Laptop/Laptop';
 import './Laptops.css'
 const Laptops = () => {
-    const laptops = [
-        { name: 'Asus ROG Zephyrus G15', price: '1550', GPU: '  Nvidia GeForce RTX 3060', CPU: 'Intel Core i9-11900H', id: '1' },
-        { name: 'Alienware m17 R4', price: '1550', GPU: ' Nvidia RTX 3080', CPU: 'Intel Core i9-10980HK', id: '2' },
-        { name: 'Dell G3 15', price: '1550', GPU: ' Nvidia GTX 1660 Ti Max-Q', CPU: ' Intel Core i7-9750H', id: '3' },
-        { name: 'Asus ROG Flow X13', price: '1550', GPU: ' Nvidia GTX 1650 Max-Q/ Nvidia RTX 3080', CPU: ' AMD Ryzen 9 5980HS', id: '4' }
-    ]
+    // const laptops = [
+    //     { name: 'Asus ROG Zephyrus G15', price: '1550', GPU: '  Nvidia GeForce RTX 3060', CPU: 'Intel Core i9-11900H', id: '1' },
+    //     { name: 'Alienware m17 R4', price: '1550', GPU: ' Nvidia RTX 3080', CPU: 'Intel Core i9-10980HK', id: '2' },
+    //     { name: 'Dell G3 15', price: '1550', GPU: ' Nvidia GTX 1660 Ti Max-Q', CPU: ' Intel Core i7-9750H', id: '3' },
+    //     { name: 'Asus ROG Flow X13', price: '1550', GPU: ' Nvidia GTX 1650 Max-Q/ Nvidia RTX 3080', CPU: ' AMD Ryzen 9 5980HS', id: '4' }
+    // ]
+    const [laptops, setLaptops] = useState([])
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setLaptops(data))
+    }, [])
     return (
         <div>
             <h2>Best gaming laptop</h2>
