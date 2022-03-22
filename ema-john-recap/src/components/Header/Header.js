b68b794900a2d5c08c419e2ae9cfa774d80e00e2
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-22 21:51:17 +0600

    header ema john ecap

--- ema-john-recap/src/components/Header/Header.js
+++ ema-john-recap/src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import logo from '../../images/Logo.svg'
+import './Header.css'
 const Header = () => {
     return (
         <div>
             <div className="header-container">
-                <img src={logo} alt="" />
+                <a href="/shop"><img src={logo} alt="" /></a>
+                <nav>
+                    <ul>
+                        <li><a href="/shop">Shop</a></li>
+                        <li><a href="/order">Order</a></li>
+                        <li><a href="/review">Order Review </a></li>
+                        <li><a href="/inventory">Manage Inventory </a></li>
+                    </ul>
+                </nav>
             </div>
         </div>
     );
